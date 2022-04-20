f07bb860f9cb88b4e211adc1d554e2082f89b1a3
Chaarangan <charangan.18@cse.mrt.ac.lk>
2022-04-20 23:30:24 +0530

    setup for emotional

---
+++ flask_app/wsgi.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer
+from app import server
+from flask import current_app
+from flask_app.app import BERTClass
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+import torch
+
+if __name__ == "__main__":
+    with server.app_context():
+        current_app.model = BERTClass(6)
+        current_app.model.to(device)
+        current_app.model.load_state_dict(torch.load(
+            './model/model.bin', map_location=torch.device('cpu')))
+        current_app.tokenizer = AutoTokenizer.from_pretrained(
+            "./model/tokenizer/")
+    # config param
+    server.run(host='0.0.0.0', port=8000, debug=True)
+
